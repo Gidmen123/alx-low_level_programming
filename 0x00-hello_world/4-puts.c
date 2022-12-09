60304450f25c150bcf9742293aad9058938e84f1
Gideon Mensah <gomy1233@gmail.com>
2022-12-08 21:54:29 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

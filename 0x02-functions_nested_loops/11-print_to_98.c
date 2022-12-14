a3d524f86d687299834ce4855e5127cde9729f7c
Gideon Mensah <gomy1233@gmail.com>
2022-12-13 19:38:16 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - print all numbers from n to 98
@@ -10,12 +10,12 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%i, ", n);
+		printf("%d, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		printf("%i, ", n);
+		printf("%d, ", n);
 		n--;
 	}
 	printf("98");

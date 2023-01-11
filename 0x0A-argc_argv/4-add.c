ae016764757be88ee37ebf8a54eb005da035043e
Gideon Mensah <gomy1233@gmail.com>
2023-01-10 17:21:17 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,19 +4,18 @@
 #include <string.h>
 
 /**
- * check_num - check string number
+ * check_num - check - string there are digti
  * @str: array str
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int check_num(char *str)
-
 {
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str))
 
+	while (count < strlen(str))
 	{
 		if (!isdigit(str[count]))
 		{
@@ -24,16 +23,15 @@ int check_num(char *str)
 		}
 		count++;
 	}
-
 	return (1);
 }
 
 /**
- * main - Print the name of the program
+ * main - Print the mane of the program
  * @argc: Count arguments
- * @argv: Auguments
+ * @argv: Arguments
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
@@ -42,22 +40,21 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	count = 1;
+
 	while (count < argc)
 	{
-	if (check_num(argv[count]))
-				{
+		if (check_num(argv[count]))
+		{
 			str_to_int = atoi(argv[count]);
-				sum += str_to_int;
-				}
-
-				else
-				{
-				printf("Error\n");
-				return (1);
-				}
-
-	count++;
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
 	printf("%d\n", sum);
 	return (0);
-	}
+}
